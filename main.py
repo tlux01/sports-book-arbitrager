c88452aab43d2ca9e0927354e354f11dd19e0c10
tim3dwards3 <45614991+tim3dwards3@users.noreply.github.com>
2020-12-06 02:53:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -41,37 +41,53 @@ def sports_book_scraper():
 
 #sports_book_scraper()
 
-#url of nfl picks
-url = 'https://www.sportsplays.com/pick/eventList/sport_id/1.html'
+def sports_plays_scraper():
+    url = 'https://www.sportsplays.com/pick/eventList/sport_id/1.html'
 
-    # sportsbook ncaa basketball
-    # url = 'https://www.sportsbook.com/sbk/sportsbook4/ncaa-football-betting/game-lines.sbk'
-s = requests.session()
+        # sportsbook ncaa basketball
+        # url = 'https://www.sportsbook.com/sbk/sportsbook4/ncaa-football-betting/game-lines.sbk'
+    s = requests.session()
 
-login_data = {
-    'username' : 'youruser',
-    'password': 'yourpass',
-}
-response = s.post(url, data=login_data)
-r = s.get('https://www.sportsplays.com/pick/eventList/sport_id/1.html')
-soup = BeautifulSoup(r.content, 'html.parser')
-bet_table = soup.find(id = 'ajax_tabs_event_list')
-tr = bet_table.findAll('tr')
-td = tr[3].findAll('td')
-#rows = bet_table.findAll('tr')
+    login_data = {
+        'username' : 'edward30@my.canisius.edu',
+        'password': 'spo12344te',
+    }
+    response = s.post(url, data=login_data)
 
-#cut out extra will want to make dynamic eventually
-#date row starts at tr[3] and ends len(tr)-2
-i = 3
-table_end = len(tr) - 1
-print(table_end)
+#entry point for url
 
-#break up only tr you need: i is start and table_end is the end
-while i < table_end:
-    print(i)
-    i += 1
+    r = s.get('https://www.sportsplays.com/pick/eventList/sport_id/1.html')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    bet_table = soup.find(id = 'ajax_tabs_event_list')
+    tr_s = bet_table.findAll('tr')
+    #starting point of table is 3
+    i = 3
+    #might be minus 2
+    table_end = len(tr_s) - 1
+    #total rows
+    print(table_end)
 
-#print(len(tr))
-#print(tr[57].text)
-#print(td[0].text)
-sports_book_scraper()
+    date = None
+    game_odds = []
+    line = None
+    while i < table_end:
+        td_s = tr_s[i].findAll('td')
+        if len(td_s) < 2:
+            game_date = td_s[0].text
+        else:
+            date = game_date
+            teams = td_s[1].text.strip()
+            point_spreads = td_s[2].text.strip()
+            over_under = td_s[3].text.strip()
+            moneyline = td_s[4].text.strip()
+            line = {
+                "date": date, "teams": teams, "point spread": point_spreads,
+                "moneyline": moneyline, "O/U": over_under
+            }
+            game_odds.append(line)
+        i += 1
+
+    print(game_odds[6]['O/U'])
+    return game_odds
+
+sports_plays_scraper()
